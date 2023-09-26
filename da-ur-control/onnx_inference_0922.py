1cb05c6fdf45c205d52973e16506cb6f3206d0b4
pzhang18 <pengfeizhang@hotmail.com>
2023-09-26 23:30:38 +0200

    convert tcp orientation to unity rotation

--- da-ur-control/onnx_inference_0922.py
+++ da-ur-control/onnx_inference_0922.py
@@ -3,23 +3,36 @@ import numpy as np
 from scipy.spatial.transform import Rotation
 
 # Load the ONNX model
-model_path = "C:/Users/pengf/rl/RTDE_Python_Client_Library/examples/TCP23SAC.onnx"
+model_path = "C:/Users/pengf/rl/da-ur-control/NN/SAC04_500k.onnx"
 ort_session = ort.InferenceSession(model_path, providers=['CPUExecutionProvider'],)
 
 # Prepare input data with 22 parameters 
 # REPLACE WITH ACTUAL RTDE VALUES
 obs_target_pos = [0.00424165790900588, 0.019999999552965164, 0.013266406953334808,] # Manually measure vector3
+obs_target_orientation = [0.00020356, -0.00044415,  0.00088845] # vector3
 obs_position = [0.0015319063095375896, 0.09820583462715149, -0.00041362192132510245, ] # vector3
-obs_rotation_v3 = [0.00020356, -0.00044415,  0.00088845] # vector3
+obs_orientation = [0.00020356, -0.00044415,  0.00088845] # vector3
 obs_force = [-1.3748521269008052e-05, -9.809981346130371, 4.723733582068235e-06, ] # vector3
 obs_torque = [-0.0006215430330485106, 1.2438973726602853e-06, 0.4888324439525604,] # vector3
 obs_velocity = [0.0, 0.0, 0.0,] # vector3
 obs_ang_velocity = [-0.0011297821765765548, -0.0024679843336343765, 0.004935592878609896] # vector3
 
-# convert XYZ rotation to Unity's quaterion coordinate. 
-obs_rotation = Rotation.from_euler('xyz', obs_rotation_v3, degrees=False).as_quat().tolist()
+def to_rotation(vector):
+    rx = vector[:, 0]
+    ry = vector[:, 1]
+    rz = vector[:, 2]
+    rotation_x_deg = np.degrees(np.arctan2(ry, rz)).reshape(-1, 1)
+    rotation_y_deg = np.degrees(np.arctan2(rx, rz)).reshape(-1, 1)
+    rotation_z_deg = np.degrees(np.arctan2(ry, rx)).reshape(-1, 1)
+    rotation = np.hstack((rotation_x_deg, rotation_y_deg, rotation_z_deg))
+    return rotation
+
+# convert tool orientation to euler rotation
+obs_rotation = to_rotation(obs_orientation)
+obs_target_rotation = to_rotation(obs_target_orientation)
+
 # concatenate all observation into an input tensor
-input_data = np.array((obs_target_pos + obs_position + obs_rotation
+input_data = np.array((obs_target_pos + obs_target_rotation + obs_position + obs_rotation
                + obs_force + obs_torque + obs_velocity + obs_ang_velocity),
                dtype=np.float32)
 input_data = input_data.reshape(1, 22)
@@ -38,3 +51,4 @@ act_velocity_z = continuous_actions[2]
 act_ang_velo_x = continuous_actions[3]
 act_ang_velo_y = continuous_actions[4]
 act_ang_velo_z = continuous_actions[5]
+
